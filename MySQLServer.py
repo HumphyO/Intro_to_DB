768e4a9820dc3b0554c98f61926f87a9eeb87595
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-10-21 22:25:19 +0300

    Added a MySQLServer.py script to create database into MySQL server

---
+++ MySQLServer.py
@@ -0,0 +1,30 @@
+import mysql.connector
+def create_database(lhost, user, password, database_name):
+    try:
+        cnx = mysql.connector.connect(
+           host = host,
+           user = user,
+           password = password
+        )
+
+        cursor = cnx.cursor()
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
+        cnx.commit()
+
+        print(f"Database '(MySQLServer.py)' created succesfully!")
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        if cursor:
+            cursor.close()
+        if cnx:
+            cnx.close()
+
+if __name__ == "__main__":
+    host = "localhost"
+    user = "root"
+    password = "Developer_@101"
+    database_name = " alx_book_store"
+
+    create_database(host, user, password, database_name)
