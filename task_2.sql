c97f20c70a02b41210afb84db142d375fd3785b5
Humphrey Osoro <humphreymwebi18@gmail.com>
2024-10-26 23:20:39 +0300

    Modified the task_2.sql script for creating tables

--- task_2.sql
+++ task_2.sql
@@ -1,5 +1,28 @@
-CREATE TABLE books();
-CREATE TABLE authors();
-CREATE TABLE customers();
-CREATE TABLE orders();
-CREATE TABLE order details();
+CREATE TABLE books(
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT, 
+    price DOUBLE,
+    publication_date DATE
+    );
+CREATE TABLE authors(
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+CREATE TABLE customers(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+CREATE TABLE orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE
+);
+CREATE TABLE order details(
+    order_detail_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE
+);
